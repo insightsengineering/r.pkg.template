8bd625c4bdb50401d9fad3a502d79e0a87f31fec
Dinakar <26552821+cicdguy@users.noreply.github.com>
2022-09-01 05:35:33 -0500

    Update testing strategy (#104)

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,10 +1,3 @@
 pkg_name <- "r.pkg.template"
-if (requireNamespace("testthat", quietly = TRUE)) {
-  library(testthat)
-  reporter <- MultiReporter$new(list(
-    CheckReporter$new(),
-    JunitReporter$new(file = "junit-result.xml")
-  ))
-  test_results <- test_check(pkg_name, reporter = reporter)
-  saveRDS(test_results, "unit_testing_results.rds")
-}
+library(pkg_name, character.only = TRUE)
+testthat::test_check(pkg_name)
